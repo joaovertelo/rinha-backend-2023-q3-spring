898146b81d96974a8eb946e64e3555c31ab743ce
João Vertelo <joaovertelo@gmail.com>
2023-08-22 20:05:54 -0300

    ajuste script

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -3,9 +3,9 @@ DROP TABLE IF EXISTS public.pessoa;
 CREATE TABLE public.pessoa (
 	id uuid NOT NULL,
 	apelido varchar(32) NOT NULL,
-	nascimento varchar(10) NULL,
+	nascimento varchar NULL,
 	nome varchar(100) NOT NULL,
-	stack varchar(255) NULL,
+	stack varchar NULL,
 	CONSTRAINT pessoa_pkey PRIMARY KEY (id),
 	CONSTRAINT pessoa_apelido_constraint UNIQUE (apelido)
 );
